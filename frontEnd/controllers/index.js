b9b10dc0055f62b932c3d160b53c6ddc38a4b75f
facundo2309 <facundoomargarcia@gmail.com>
2022-11-14 16:57:03 -0300

    fix font size + add resposive form

--- frontEnd/controllers/index.js
+++ frontEnd/controllers/index.js
@@ -1,5 +1,8 @@
-const navigateToSecction = (idSecction) =>{
-    console.log(location)
+const navigateToSecction = (idSecction) => {
     location.hash = "#" + idSecction;
-    
+    return
+}
+
+const naviateLogin = () => {
+    location.href = "../frontEnd/components/login.html"
 }
