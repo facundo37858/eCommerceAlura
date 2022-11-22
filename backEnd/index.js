40d143deb4905530246eeb9d70a79817f64af95a
facundo2309 <facundoomargarcia@gmail.com>
2022-11-22 17:04:16 -0300

    crud search

--- backEnd/index.js
+++ backEnd/index.js
@@ -2,4 +2,15 @@ import { searchProduct } from "./controllers/search.js"
 
 const request = await searchProduct('YYYY')
 
-console.log(request)
+const searchButton = document.querySelector('[data-icon-serach]')
+
+searchButton.addEventListener('click',async ()=>{
+    const searchValue = document.querySelector('[data-input-search]')
+
+    if(searchValue.value){
+        const data = await searchProduct(searchValue.value.toUpperCase())
+        data.length ?
+        console.log(data):
+        console.log('NOT FOUND')
+    }
+})
