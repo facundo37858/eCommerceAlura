3b8f28601badc5407f14c7f55fa1517ae6a5533f
facundo2309 <facundoomargarcia@gmail.com>
2022-11-23 16:02:34 -0300

    submit add product

--- frontEnd/controllers/formAddProduct/submit.js
+++ frontEnd/controllers/formAddProduct/submit.js
@@ -1,4 +1,33 @@
-export const submit = ()=>{
+import { addProduct } from "../../../backEnd/controllers/addProduct.js"
+
+export const submit = async ()=>{
     const inputs = document.querySelectorAll('input')
-    console.log(inputs)
+    const description = document.querySelector('[data-tipo="descripcion"]')
+    const auxObj = {
+        nameProduct:'',
+        descripcion:description.value,
+        precio:'',
+        file:'',
+        categoria:'',
+        id:uuid.v4()
+    }
+
+    inputs.forEach(input=>{
+        const typeInput = input.dataset.tipo
+        if(typeInput){
+            auxObj[typeInput] = input.value
+
+        }
+    })
+    await addProduct(
+        auxObj.nameProduct,
+        auxObj.descripcion,
+        auxObj.precio,
+        auxObj.file,
+        auxObj.categoria
+    )
+
+    return
+   
+    
 }
