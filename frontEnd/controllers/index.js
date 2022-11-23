3951f24b32a6dd4d68e50547c1f5b5c92ed41018
facundo2309 <facundoomargarcia@gmail.com>
2022-11-23 16:02:53 -0300

    link to home

--- frontEnd/controllers/index.js
+++ frontEnd/controllers/index.js
@@ -6,3 +6,8 @@ const navigateToSecction = (idSecction) => {
 const naviateLogin = () => {
     location.href = "components/login.html"
 }
+
+const linkToHome = document.querySelector('[data-link-home]')
+linkToHome.addEventListener('click',()=>{
+    if(location.pathname !== '/frontEnd/index.html') location.href = "../index.html"
+})
